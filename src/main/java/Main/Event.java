f911530506515e7b314b516b6d63ddc89708754b
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2019-02-26 18:46:53 +0200

    new features

--- src/main/java/Main/Event.java
+++ src/main/java/Main/Event.java
@@ -3,6 +3,9 @@ package Main;
 import org.springframework.stereotype.Component;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.Random;
 
@@ -11,14 +14,17 @@ public class Event {
     int id;
     Date date;
     DateFormat dateFormat;
-
+    static LocalTime time = LocalTime.now();
 
     public Event(Date date, DateFormat dateFormat) {
         Random random = new Random();
         id = random.nextInt(100);
         this.date = date;
         this.dateFormat = dateFormat;
+    }
 
+    public static boolean isDay(int start, int end) {
+        return time.getHour() > start && time.getHour() < end;
     }
 
     @Override
--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,82 +1,30 @@
 import Main.Event;
 import Main.EventLogger;
+import javafx.scene.input.DataFormat;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.FieldPosition;
-import java.text.ParsePosition;
+import java.text.*;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class MainTest {
-    int cacheSize = 3;
-    Date date;
-    DateFormat dateFormat;
-    Event event = new Event(date,dateFormat);
-    static List<Integer>  cache;
-
-   static File file = new File("D:\\ipLogFile.txt");
-    static boolean fileCreated = file.canWrite();
-public static void test(){
-    System.out.println(fileCreated);
-}
+    static int time3 = DateFormat.HOUR_OF_DAY1_FIELD;
+    static int time2 = DateFormat.HOUR_OF_DAY1_FIELD;
+    static int time1 = DateFormat.HOUR_OF_DAY0_FIELD;
+    static LocalTime time = LocalTime.now();
 
     public static void main(String[] args) {
-    test();
-
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        boolean fileCreated = file.canWrite();
-        System.out.println(fileCreated);
-        MainTest.cache= new ArrayList<Integer>();
-
-
-       /* Date date = new Date();
-        DateFormat dateFormat = new DateFormat() {
-            @Override
-            public StringBuffer format(Date date, StringBuffer toAppendTo, FieldPosition fieldPosition) {
-                return null;
-            }
-
-            @Override
-            public Date parse(String source, ParsePosition pos) {
-                return null;
-            }
-        };*/
-        int cacheSize = 3;
-
-//        Event event = new Event(date,dateFormat);
-        cache.add(12);
-        cache.add(12);
-        cache.add(12);
-        TestMethod();
-        cache.add(12);
-        TestMethod();
-        cache.add(12);
-        TestMethod();
-
-
+        System.out.println(time1);
+        System.out.println(time2);
+        isDay(5,17);
     }
+    public static boolean isDay(int start, int end) {
+        int currentTime = time.getHour();
 
-    public static void TestMethod (){
-        if (cache.size() == 3) {
-            writeEventsFromCache(cache);
-            System.out.println("Logs were written to file (3 logs)");
-            cache.clear();
-        }
-    }
-    public static void writeEventsFromCache(List cache) {
-        try {
-            FileUtils.writeStringToFile(file, String.valueOf(cache), "utf-8", true);
-            System.out.println("Log was written successfully");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return time.getHour() > start && time.getHour() < end;
     }
     }
